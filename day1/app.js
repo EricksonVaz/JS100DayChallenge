f9f0c2c2b3dbbc2b4056846983f8e4827ec7c1eb
erickson <ericksoncv2@gmail.com>
2020-10-11 11:53:05 -0100

    Day 1-Challenge JS change background color

---
+++ day1/app.js
@@ -0,0 +1,14 @@
+var btn = document.querySelector('.btn');
+var body = document.querySelector('body');
+
+var colors = [
+    'orchid',
+    'blueviolet',
+    'royalblue',
+    'palevioletred',
+    'tomato'
+]
+
+btn.addEventListener('click',function(){
+    body.style.backgroundColor = colors[parseInt(Math.random() * colors.length)];
+});
